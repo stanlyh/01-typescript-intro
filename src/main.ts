5f28bcc59bf0d22fe019ecfd76c70a19bfc95591
stanlyh <eduardostanly@gmail.com>
2025-02-26 22:49:53 -0500

    tipos basicos typescript - vite

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+//import './topics/01-basic-types';
+
+const app =  document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `Hola Celeste Kat, Evangeline`;
+
+console.log('Hola Cristal');
---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,14 @@
+
+
+const name: string = 'Garfield';
+let hpPoints: number | 'FULL' = 77;
+const isAlive: boolean = true;
+
+
+hpPoints = 'FULL';
+
+console.log({
+    name, hpPoints, isAlive
+})
+
+export {};
