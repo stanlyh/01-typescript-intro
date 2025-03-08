f039932d159bbe6fa98f3aa817e08df442e42f45
stanlyh <eduardostanly@gmail.com>
2025-03-07 23:33:47 -0500

    Ejemplos interfaces

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 
 //import './topics/01-basic-types';
 //import './topics/02-object-interface';
-import './topics/03-functions';
+//import './topics/03-functions';
+import './topics/04-homework-types';
 
 const app =  document.querySelector<HTMLDivElement>('#app')!;
 
--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -13,13 +13,35 @@ function multiply( firstNumber:number, secondNumber?:number, base:number = 2 ) {
 }
 
 
+//const result: number = addNumbers(7, 9);
+//const result2: string = addNumbersArrow(7, 9);
+//const multiplyResult: number = multiply(5);
+//console.log({ result, result2, multiplyResult });
 
-const result: number = addNumbers(7, 9);
-const result2: string = addNumbersArrow(7, 9);
-const multiplyResult: number = multiply(5);
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
 
-console.log({ result, result2, multiplyResult });
+const healCharacter = ( character: Character, amount: number ) => {
+
+    character.hp += amount;
+
+}
+
+const mazinger: Character = {
+    name: 'Mazinger',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${ this.hp }`);
+    },
+}
 
 
+healCharacter( mazinger, 12 );
+healCharacter( mazinger, 25 );
+
+mazinger.showHp();
 
 export {};
